101a86408f5cbd5f91eec123687248b6189069d8
maryam-taer <57878026+Maryam-Taer@users.noreply.github.com>
2021-11-04 11:14:51 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -19,22 +19,19 @@ with open('log_conf.yml', 'r') as f:
 
 logger = logging.getLogger('basicLogger')
 
-
 def populate_stats() -> NoContent:
     """ Periodically update stats """
     storage_url = app_config["eventstore"]["url"]
     logger.info("Started Periodic Processing!")
 
     read_from_json()
-    print(read_from_json())
     rest_record = get_restaurant_records(storage_url)
     review_record = get_review_records(storage_url)
-    calc_stat(rest_record, review_record)
+    calc_stat(rest_record[0], review_record[0], rest_record[1], review_record[1])
 
     logger.debug("Periodic Processing has ended!")
 
     return NoContent, 200
-
 
 def get_stats():
     logger.info("Started statistic request Process!")
@@ -51,16 +48,18 @@ def get_stats():
 
     return data, 200
 
-
 def get_restaurant_records(s_url):
     try:
         # http://localhost:8090/finding-restaurant?timestamp="2021-10-14T13:14:50Z"
         headers = {"Content-Type": "application/json"}
-        response = requests.get(f'{s_url}finding-restaurant?timestamp={read_from_json()["last_updated"]}',
-                                headers=headers)
+        # current_datetime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        response = requests.get(f'{s_url}finding-restaurant?timestamp={read_from_json()["last_updated"]}', headers=headers)
         json_data = json.loads(response.text)
+        total_events = len(json_data)
+
         logger.info(
-            f'Query for restaurant search records after {read_from_json()["last_updated"]} returns {len(json_data)} '
+            f'Query for restaurant search records after {read_from_json()["last_updated"]} returns {total_events} '
             f'results.')
 
         if not response.status_code // 100 == 2:
@@ -73,19 +72,22 @@ def get_restaurant_records(s_url):
         # A serious problem happened, like an SSLError or InvalidURL
         return f"Error: {e}"
 
-    return json_data
-
+    return [json_data, total_events]
 
 def get_review_records(s_url):
     try:
         # HTTP GET method to get "restaurant search" records from the Storage DB.
         # http://localhost:8090/wrinting-review?timestamp="2021-10-14T13:14:50Z"
         headers = {"Content-Type": "application/json"}
+        # current_datetime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
         response = requests.get(f'{s_url}wrinting-review?timestamp={read_from_json()["last_updated"]}', headers=headers)
 
         json_data = json.loads(response.text)
+        total_events = len(json_data)
+
         logger.info(
-            f'Query for review records after {read_from_json()["last_updated"]} returns {len(json_data)} results.')
+            f'Query for review records after {read_from_json()["last_updated"]} returns {total_events} results.')
 
         if not response.status_code // 100 == 2:
             return logger.debug(
@@ -97,8 +99,7 @@ def get_review_records(s_url):
         # A serious problem happened, like an SSLError or InvalidURL
         return f"Error: {e}"
 
-    return json_data
-
+    return [json_data, total_events]
 
 def read_from_json():
     offset_time = datetime.datetime.now() - datetime.timedelta(minutes=15)
@@ -140,20 +141,22 @@ def write_to_json(takeout, reviews, all_rest_record, all_review_record):
     return file
 
 
-def calc_stat(restr_data, review_data):
+def calc_stat(restr_data, review_data, rest_len, review_len):
     """ Calculate stats for restaurant searched records filtered by timestamp """
     takeout_counter = 0
     top_review_counter = 0
 
-    all_rest_records = len(restr_data)
-    all_review_records = len(review_data)
+    all_rest_records = int(rest_len)
+    all_review_records = int(review_len)
 
     for record in restr_data:
+        # record = record.decode("utf-8")
         # print(record)
         if record["Delivery_option"] == "takeout":
             takeout_counter += 1
 
     for record in review_data:
+        # record = record.decode("utf-8")
         if record["Rate_no"] == 5:
             top_review_counter += 1
 
@@ -173,4 +176,4 @@ app.add_api(YAML_FILE, strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     # run our standalone gevent server
     init_scheduler()
-    app.run(port=8100, use_reloader=False)
+    app.run(port=8100, host="0.0.0.0", use_reloader=False)
