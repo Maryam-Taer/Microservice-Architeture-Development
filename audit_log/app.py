0344ec51cd887c6378df645ec1607744f69524bb
maryam-taer <57878026+Maryam-Taer@users.noreply.github.com>
2021-11-06 13:42:58 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -86,6 +86,8 @@ def get_posted_reviews(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
