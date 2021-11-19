d0dc1ace8986447a9feebf96a382ca6b0dd14a52
maryam-taer <57878026+Maryam-Taer@users.noreply.github.com>
2021-11-18 18:43:45 -0800

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 import yaml
 import logging
@@ -11,14 +12,27 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 YAML_FILE = "openapi.yaml"
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info(f"App Conf File: {app_conf_file}")
+logger.info(f"Log Conf File: {log_conf_file}")
 
 def populate_stats() -> NoContent:
     """ Periodically update stats """
